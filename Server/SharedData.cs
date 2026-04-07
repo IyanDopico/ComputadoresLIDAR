50ecbfa5b2887b5ee6f0ec82acd09a91f347106b
Iyán Dopico <89909202+IyanDopico@users.noreply.github.com>
2026-04-07 19:40:50 +0200

    Delete Server/SharedData.cs

--- Server/SharedData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public static class SharedData
-{
-    public static double[] CurrentMap = null;
-    public stattic bool Running=false;
-    public static readonly object Maplock=new object();//prueba
-}
