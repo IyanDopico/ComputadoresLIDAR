ac01763a80aca3314b19ab60b1aef198b19cca82
KarlaCGuaracoF <kcguaraco@gmail.com>
2026-04-07 19:32:03 +0200

    shared data otra vez

--- Server/SharedData.cs
+++ Server/SharedData.cs
@@ -4,5 +4,5 @@ public static class SharedData
 {
     public static double[] CurrentMap = null;
     public stattic bool Running=false;
-    public static readonly object Maplock=new object();
+    public static readonly object Maplock=new object();//prueba
 }
