e4f312e76c44084c2f6128ec5dcb145df3f6fe64
Iyán Dopico <iyan.dopico@gmail.com>
2026-04-07 19:44:05 +0200

    shareddata

---
+++ Server/SharedData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+
+    public static class SharedData
+    {
+        public static double[] CurrentMap = null;
+        public static bool Running = false;
+        public static readonly object Maplock = new object();
+    }
+
+}
